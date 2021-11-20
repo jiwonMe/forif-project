b5846764eb12e2e34832473fc6c3ec67e18c7347
monognuisy <ysmsmart1@gmail.com>
2021-11-20 15:59:41 +0900

    timechecker test

--- frontend/src/pages/TimeChecker.tsx
+++ frontend/src/pages/TimeChecker.tsx
@@ -1,3 +1,16 @@
 // 시간 체크 페이지입니다.
+import React, { useState } from 'react';
 
-export{}
+const TimeChecker = () => {
+  const [timeElapsed, setTimeElapsed] = useState(0);
+
+  return (
+    <div className="timeChecker">
+      <h1>{timeElapsed}</h1>
+      <button>Start</button>
+      <button>Stop</button>
+    </div>
+  );
+};
+
+export default TimeChecker;
